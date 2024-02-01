d46b7241143fe7248b7f8ecc6ba208915917e4f8
AnandRPSICS <anandrp.sics@gmail.com>
2024-02-01 19:29:46 +0530

    navbar design completd

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/common/Home/Home";
 
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
-        
+
           <Route path="/*" element={"<h1> 404 </h1>"} />
         </Routes>
       </BrowserRouter>
--- src/components/common/userNavbar/userNavbar.jsx
+++ src/components/common/userNavbar/userNavbar.jsx
@@ -2,8 +2,20 @@ import { Container } from "react-bootstrap";
 import navStyle from "./userNavbar.module.css";
 const UserNavbar = () => {
   return (
-    <Container className={navStyle.navContainer} fluid>
-      <h1> User navbar</h1>
+    <Container
+      className={`${navStyle.navContainer} center d-flex align-items-center px-5`}
+      fluid
+    >
+      <h3 className="m-0 me-5"> Trade Hub</h3>
+      <ul className="list-unstyled m-0 d-flex cursor-pointer d-flex align-items-center justify-content-between">
+        <li>Home</li>
+        <li>Guide</li>
+        <li>Markets</li>
+      </ul>
+
+      <div className={`${navStyle.navRegisterContainer} ms-auto`}>
+        <button className="w-100 border-0 btn">Login/Register</button>
+      </div>
     </Container>
   );
 };
